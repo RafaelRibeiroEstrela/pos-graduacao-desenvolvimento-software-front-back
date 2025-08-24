5fd73beaba747b2090fa38d00d79cec76238db9b
Rafael Ribeiro Estrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2025-08-24 18:51:42 -0400

    Ajustando tela principal

--- projeto-final/portal-produtos-angular/src/app/component/product-form/product-form.component.ts
+++ projeto-final/portal-produtos-angular/src/app/component/product-form/product-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {ProductRequest} from '../../models/product-request.model';
 import {ProductClientService} from '../../client/product-client.service';
 import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -12,21 +13,27 @@ import {FormsModule} from '@angular/forms';
   styleUrl: './product-form.component.css'
 })
 export class ProductFormComponent {
-  name!: string;
-  description!: string;
-  price!: number;
-  pictureUrl!: string;
-  category!: string;
+  product: ProductRequest = {
+    name: "",
+    description: "",
+    price: 0,
+    pictureUrl: "",
+    category: "",
+  };
 
-  submit(product: ProductRequest): void {
-    this.productClient.create(product);
-    //JOGAR PARA A ROTA DE LISTAR PRODUTOS
+  submit(productRequest: ProductRequest): void {
+    this.productClient.create(productRequest).subscribe({
+      next: () => {
+        this.router.navigate(['/products']);
+      },
+      error: (err) => console.error('Erro ao atualizar:', err)
+    });
   }
 
   handleCancel(): void {
-    //JOGAR PARA A ROTA DE LISTAR PRODUTOS
+    this.router.navigate(['/products']);
   }
 
-  constructor(private productClient: ProductClientService) {
+  constructor(private productClient: ProductClientService, private router: Router) {
   }
 }
